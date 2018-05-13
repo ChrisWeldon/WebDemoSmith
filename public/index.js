2710101c16f4f9d830d98e93773ba6cd5fcc5c15
Chris Evans <cwevans612@gmail.com>
2018-05-12 21:39:44 -0400

    Rotation on objects works now

--- public/index.js
+++ public/index.js
@@ -1,50 +1,53 @@
 
 if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 var camera, controls, scene, renderer, stats;
+var dials  = []
 init();
 animate();
 function init() {
   var objloader = new THREE.OBJLoader();
-  var matloader = new THREE.MaterialLoader();
   var len_multiplier = 0
+  var geometry = new THREE.CubeGeometry( 1,1,1 );
+  var material = new THREE.MeshPhongMaterial( { color: 0x00ffff} );
   for(var i = 0; i<10; i++){
-
     objloader.load(
       // resource URL
       'assets/dial_mtl.obj',
       // called when resource is loaded
       function ( object ) {
+        mesh = new THREE.Mesh(geometry, material  )
+        mesh.position.z = mesh.position.z + 1.15*len_multiplier - 1.15*4
+        mesh. position.x = mesh.position.x + 6
+        scene.add(mesh)
+        dials.push(mesh)
         console.log(object)
         object.position.z = object.position.z + 1.15*len_multiplier - 1.15*4
         console.log(object.position.z)
         object.updateMatrix();
         object.matrixAutoUpdate = false
         scene.add(object)
+        dials.push(object)
         len_multiplier++
       },
       // called when loading is in progresses
       function ( xhr ) {
-
         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
       },
       // called when loading has errors
       function ( error ) {
-
         console.log( 'An error happened' );
-
       }
     );
   }
 
   camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.z = 500;
+  camera.position.z = 15;
   controls = new THREE.TrackballControls( camera );
   controls.rotateSpeed = 1.0;
   controls.zoomSpeed = 1.2;
   controls.panSpeed = 0.8;
   controls.noZoom = false;
-  controls.noPan = false;
+  controls.noPan = true;
   controls.staticMoving = true;
   controls.dynamicDampingFactor = 0.3;
   controls.keys = [ 65, 83, 68 ];
@@ -53,17 +56,7 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xcccccc );
   scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-  var geometry = new THREE.CylinderBufferGeometry( 0, 10, 30, 4, 1 );
-  var material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
-  /*for ( var i = 0; i < 500; i ++ ) {
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.z = ( Math.random() - 0.5 ) * 1000;
-    mesh.updateMatrix();
-    mesh.matrixAutoUpdate = false;
-    scene.add( mesh );
-  }*/
+
   // lights
   var light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 1, 1, 1 );
@@ -83,6 +76,71 @@ function init() {
   //
   window.addEventListener( 'resize', onWindowResize, false );
   //
+
+  document.addEventListener( 'mousedown', onDocumentMouseDown );
+  document.addEventListener('mousemove', onDocumentMouseMove);
+  document.addEventListener('mouseup', onDocumentMouseUp);
+  var intersects
+
+  var isDragging = false;
+  var previousMousePosition = {
+    x: 0,
+    y: 0
+  };
+
+  function onDocumentMouseDown( event ) {
+      var previousMousePosition = {
+        x: 0,
+        y: 0
+      };
+      event.preventDefault();
+      var mouse3D = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1,-( event.clientY / window.innerHeight ) * 2 + 1,  0.5 );
+      var raycaster =  new THREE.Raycaster();
+      raycaster.setFromCamera( mouse3D, camera );
+      intersects = raycaster.intersectObjects( dials );
+      console.log(intersects)
+      if ( intersects.length > 0 ) {
+          isDragging = true
+          controls.rotateSpeed = 0.0;
+      }
+      render();
+  }
+
+  function onDocumentMouseMove(event){
+    event.preventDefault();
+
+    var deltaMove = {
+      x: event.offsetX-previousMousePosition.x,
+      y: event.offsetY-previousMousePosition.y
+    };
+    if(isDragging) {
+      console.log("Dragging")
+      var deltaRotationQuaternion = new THREE.Quaternion()
+      .setFromEuler(new THREE.Euler(
+        0,//toRadians(deltaMove.y * 1),
+        0,//toRadians(deltaMove.x * 1),
+        toRadians((deltaMove.y-deltaMove.x)*1),
+        'XYZ'
+      ));
+
+      intersects[0].object.quaternion.multiplyQuaternions(deltaRotationQuaternion, intersects[0].object.quaternion);
+    }
+
+    previousMousePosition = {
+      x: event.offsetX,
+      y: event.offsetY
+    };
+
+    render();
+  }
+
+  function onDocumentMouseUp( event ){
+    intersects = []
+    isDragging = false
+    controls.rotateSpeed = 1.0;
+    render();
+  }
+
   render();
 
 }
@@ -103,4 +161,12 @@ function animate() {
 function render() {
   renderer.render( scene, camera );
   stats.update();
+}
+
+function toRadians(angle) {
+	return angle * (Math.PI / 180);
+}
+
+function toDegrees(angle) {
+	return angle * (180 / Math.PI);
 }
